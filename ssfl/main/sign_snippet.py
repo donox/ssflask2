f719058a0cb9455772aa6b3dc758c28212f1030e
don@theoxleys.com <don@theoxleys.com>
2020-05-10 12:29:36 -0400

    Add styling, template support for notices/signs.

--- ssfl/main/sign_snippet.py
+++ ssfl/main/sign_snippet.py
@@ -4,13 +4,19 @@ class Sign(object):
     """The sign object represents a simple item for display such as a notice, alert, quote, photo, ...
 
     """
+    foo = {"SIGN_SNIPPET": None, "node_name": "SIGN_SNIPPET", "name": None, "width": None,
+                                      "height": None, "sign_type": None, "sign_background": None, "sign_border": None,
+                                      "content": None, "styling": None}
+    sign_types = ['Notice', 'Quote', 'Humor']
+    sign_backgrounds = ['Solid', 'Gradient', 'Picture']
+    sign_borders = ['Solid', 'Gradient']
+
     def __init__(self, db_exec: DBExec):
         self.db_exec = db_exec
-        self.sign_data = dict()
         self.json_mgr = db_exec.create_json_manager()
 
-        self.sign_data['content'] = "THIS IS A NOTICE"
-
-    def create_notice(self, text_content):
-        self.sign_data['content'] = text_content
+    def create_notice(self, elem):
+        sign_descriptor = self.json_mgr.get_json_from_name('f_sign_test')
+        elem['content'] = sign_descriptor['content']
+        elem['title'] = None
 
