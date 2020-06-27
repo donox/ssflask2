784f69067fed8890c1572e155f6d7e6fa86cd913
don@theoxleys.com <don@theoxleys.com>
2020-06-27 09:29:55 -0400

    Added ability to update/add WP photo data to wp_ngg_pictures

--- ssfl/photos/forms/manage_photos_form.py
+++ ssfl/photos/forms/manage_photos_form.py
@@ -23,10 +23,10 @@ class ManagePhotosForm(FlaskForm):
     supported_functions = [('xx', '**Select a function**'),
                            ('ph_tml', 'Get Photo Metadata to Edit'),
                            ('ph_up', 'Upload Edited Photo Metadata'),
-                           ('ph_yy', 'Net Yet Implemented')]
+                           ('ph_ngg', 'Set WP Photo URL')]
     work_function = SelectField(label='Select Function',
                                 choices=supported_functions,
-                                render_kw={"id": "js1",  "docs": docs['all']['ph_all']})
+                                render_kw={"id": "js1", "docs": docs['all']['ph_all']})
     folder = StringField('Gallery/Folder', validators=[Optional()],
                          render_kw={"class": "ph_tml ph_up", "docs": docs['all']['folder']})
     early_date = DateField(label='Earliest Date', validators=[Optional()],
@@ -37,6 +37,12 @@ class ManagePhotosForm(FlaskForm):
                                     render_kw={"class": "ph_tml", "docs": docs['all']['download_filename']})
     upload_filename = FileField('Metadata Upload File Name', validators=[Optional()],
                                 render_kw={"class": "ph_up", "docs": docs['all']['upload_filename']})
+    wp_url = StringField('Wordpress Photo Filename', validators=[Optional()],
+                         render_kw={"class": "ph_ngg", "docs": docs['all']['wp_url']})
+    wp_photo_id = IntegerField('Wordpress Photo ID', validators=[Optional()],
+                               render_kw={"class": "ph_ngg", "docs": docs['all']['wp_photo_id']})
+    sst_photo_slug = StringField('New System Photo Slug', validators=[Optional()],
+                         render_kw={"class": "ph_ngg", "docs": docs['all']['sst_photo_slug']})
 
     def validate_on_submit(self, db_exec):
         res = super().validate_on_submit()
@@ -47,5 +53,16 @@ class ManagePhotosForm(FlaskForm):
         elif self.work_function.data == 'ph_up':
             return True
         elif self.work_function.data == 'ph_yy':
+            return True
+        elif self.work_function.data == 'ph_ngg':
+            if not self.wp_url.data:
+                self.errors['wp_url'] = ['Must specify filename for Wordpress Photo']
+                return False
+            elif not self.wp_url.data:
+                self.errors['wp_photo_id'] = ['Must specify ID for Wordpress Photo']
+                return False
+            elif not self.sst_photo_slug.data:
+                self.errors['sst_photo_slug'] = ['Must specify Slug for photo in new system']
+                return False
             return True
         return False
